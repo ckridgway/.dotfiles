bfbb8207b41ea44d6cf99ae470e748af603546ee
Chris Ridgway <ckridgway@gmail.com>
2023-01-21 14:11:11 -0600

    Add zsh-completions; update oh-my-zsh script.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
-[ "$PKG_MANAGER" == "brew" ] && brew install zsh
+[ "$PKG_MANAGER" == "brew" ] && brew install zsh zsh-completions
 [ "$PKG_MAANGER" == "apt-get" ] && sudo apt-get install zsh
 
 # Install oh-my-zsh also: depenencies on git and curl or wget
 # [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh)
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
